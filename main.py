e04e35e9c72c1e19107371c488a83ae5ee949ead
Craig Fisk <craigfisk@marmalade.ai>
2025-10-26 10:35:23 -0400

    In #2, added BAND.

--- main.py
+++ main.py
@@ -2,6 +2,13 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+BANDS = [
+    {'id': 1, 'name': 'The Kinks', 'genre': 'Rock'},
+    {'id': 2, 'name': 'Aphex Twin', 'genre': 'Electronic'},
+    {'id': 3, 'name': 'Slowdive', 'genre': 'Shoegaze'},
+    {'id': 4, 'name': 'Wu-Tang Clan', 'genre': 'Hip-Hop'},    
+]
+
 @app.get('/')
 async def index() -> dict[str, str]:
     return {'Hello, World!'}
