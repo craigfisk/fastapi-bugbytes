27a4e125fc595bf8ecdd344d7bfe05902df884a0
Craig Fisk <craigfisk@marmalade.ai>
2025-10-27 16:27:36 -0400

    At end of #5, deprecated "validator" replaced now by "model_validator(mode=before)" in schemas.py. Now in api.http, can send request with Boards of Canada with genre = "ELECTRONIC" and it gets converted, by this "class BandCreate(BandBase)" in schemas.py to put up a lower-case Response.

--- schemas.py
+++ schemas.py
@@ -1,7 +1,7 @@
 # pydantic schemas
 from datetime import date
 from enum import Enum
-from pydantic import BaseModel
+from pydantic import BaseModel, validator, model_validator
 
 class GenreURLChoices(Enum):
     ROCK = 'rock'
@@ -25,7 +25,16 @@ class BandBase(BaseModel):
     albums: list[Album] = []   # Default is an empty list; also 'Album' is Album in BugBytes #3@14:04
 
 class BandCreate(BandBase):
-    pass    
+    @model_validator(mode='before')
+    def validate_genre(cls, data):
+        genre_str = data.get('genre')
+        if genre_str:
+            genre_str = genre_str.replace('-', '_').upper()
+            try:
+                data['genre'] = GenreChoices[genre_str]
+            except KeyError:
+                raise ValueError(f"Invalid genre: {data.get('genre')}")
+        return data
 
 class BandWithID(BandBase):
     id: int
