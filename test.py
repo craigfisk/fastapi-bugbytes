bfd52ef4f2854186eeac45b301108923ab83e057
Craig Fisk <craigfisk@marmalade.ai>
2025-10-30 15:34:48 -0400

    In #6, still in "test.py", adding more functions to get values from Annotated: in this case, "low" and "high".

--- test.py
+++ test.py
@@ -5,8 +5,10 @@ def double(x: Annotated[int, (0,100)]) ->  int:
     hint = type_hints['x']
     if get_origin(hint) is Annotated:
         hint_type, *hint_args = get_args(hint)
-        print(hint_type)
-        print(hint_args)
+        low, high = hint_args[0]
+        print(low, high)
+        # print(hint_type)
+        # print(hint_args)
     return x * 2
 
 result = double(11111)
