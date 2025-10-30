a86f5c8d6bf8149a29f5a25094fe80ad9e483d4b
Craig Fisk <craigfisk@marmalade.ai>
2025-10-30 15:13:29 -0400

    In #6, added "include_extras=true" to "get_type_hints()" in "test.py" and now "Annotated" and the range show up in "python ./test.py" runs.

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
-from typing import Annotated
+from typing import Annotated, get_type_hints
 
 def double(x: Annotated[int, (0,100)]) ->  int:
+    type_hints = get_type_hints(double, include_extras=True)
+    print(type_hints)
     return x * 2
 
 result = double(11111)
