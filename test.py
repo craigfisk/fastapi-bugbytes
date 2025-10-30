90848a31ff3ad658c34038d6980c3b3d8a980022
Craig Fisk <craigfisk@marmalade.ai>
2025-10-30 15:55:51 -0400

    At end of #6, in "test.py" added @wraps(func).

--- test.py
+++ test.py
@@ -1,17 +1,29 @@
+# from tokenize import Double
 from typing import Annotated, get_type_hints, get_origin, get_args
+from functools import wraps
 
-def double(x: Annotated[int, (0,100)]) ->  int:
-    type_hints = get_type_hints(double, include_extras=True)
-    hint = type_hints['x']
-    if get_origin(hint) is Annotated:
-        hint_type, *hint_args = get_args(hint)
-        low, high = hint_args[0]
-        print(low, high)
-        
-        if not (low <= x <= high):
-            raise ValueError(f"x must be between {low} and {high}") 
+def check_value_range(func):
+    @wraps(func)
+    def wrapped(x): 
+        type_hints = get_type_hints(double, include_extras=True)
+        hint = type_hints['x']
+        if get_origin(hint) is Annotated:
+            hint_type, *hint_args = get_args(hint)
+            low, high = hint_args[0]
+            print(low, high)
+            
+            if not (low <= x <= high):
+                raise ValueError(f"{x} falls outside boundary {low} - {high}") 
+            
+        return func(x)
+    return wrapped
+
+# result = double(11111)
+
+@check_value_range
+def double(x: Annotated[int, (0, 10)]) -> int:
     return x * 2
 
-result = double(11111)
+result = double(1111)
 print(result)
 
